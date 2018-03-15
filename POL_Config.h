4467ab9d1cabcd04a1e5d37e957ca3718438882a
Angelito Casagrande <angelito@angelito.com.br>
2018-03-15 19:35:48 -0300

    ESP8266 Port Map

--- POL_Config.h
+++ POL_Config.h
@@ -9,3 +9,16 @@
 
 #define mqttPublishID "Identificador do publisher"
 #define mqttSubscribeID "Identificador do subscribee"
+
+// Mapeamento de pinos do NodeMCU
+#define D0    16
+#define D1    5
+#define D2    4
+#define D3    0
+#define D4    2
+#define D5    14
+#define D6    12
+#define D7    13
+#define D8    15
+#define D9    9
+#define D10   10
